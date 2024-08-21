cd2f16506eb860586868505663120af90aac4f9a
smit jogani <smitjogani00@gmail.com>
2024-08-21 21:58:56 +0530

    Dynamic form

--- app/dashboard/content/[template-slug]/page.tsx
+++ app/dashboard/content/[template-slug]/page.tsx
@@ -1,8 +1,12 @@
+"use client";
 import React from "react";
 import FormSection from "../_components/FormSection";
 import OutputSection from "../_components/OutputSection";
 import { TEMPLATE } from "../../_components/TemplateListSection";
 import Templates from "@/app/(data)/Templates";
+import { Button } from "@/components/ui/button";
+import { ArrowLeft } from "lucide-react";
+import Link from "next/link";
 
 interface PROPS {
   params: {
@@ -11,17 +15,31 @@ interface PROPS {
 }
 
 const CreateNewContent = (props: PROPS) => {
-  const selectedTemplate: TEMPLATE | undefined   = Templates?.find(
+  const selectedTemplate: TEMPLATE | undefined = Templates?.find(
     (item) => item.slug == props.params["template-slug"]
   );
 
-  return (
-    <div className="m-2 rounded-md grid grid-cols-1 md:grid-cols-3 gap-5">
-      {/* form section */}
-      <FormSection selectedTemplate={selectedTemplate} />
+  const generateAiContent = (formData: any) => {};
 
-      {/* output section */}
-      <div className="col-span-2"><OutputSection /></div>
+  return (
+    <div>
+      <Link href={"/dashboard"}>
+        <Button className="m-2">
+          <ArrowLeft /> Back
+        </Button>
+      </Link>
+      <div className="m-2 rounded-md grid grid-cols-1 md:grid-cols-3 gap-5">
+        {/* form section */}
+        <FormSection
+          selectedTemplate={selectedTemplate}
+          userFormInput={(v: any) => generateAiContent(v)}
+        />
+
+        {/* output section */}
+        <div className="col-span-2">
+          <OutputSection />
+        </div>
+      </div>
     </div>
   );
 };
--- app/dashboard/content/_components/FormSection.tsx
+++ app/dashboard/content/_components/FormSection.tsx
@@ -1,5 +1,5 @@
-'use client'
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import { TEMPLATE } from "../../_components/TemplateListSection";
 import Image from "next/image";
 
@@ -9,13 +9,21 @@ import { Button } from "@/components/ui/button";
 
 interface PROPS {
   selectedTemplate?: TEMPLATE;
+  userFormInput:any
 }
 
-const FormSection = ({ selectedTemplate }: PROPS) => {
+const FormSection = ({ selectedTemplate, userFormInput }: PROPS) => {
+  const [formData, setFormData] = useState<any>();
 
-  const onsubmit = (e:any) => {
+  const handleInputChange = (event: any) => {
+    const { name, value } = event.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const onsubmit = (e: any) => {
     e.preventDefault();
-  } 
+    userFormInput(formData);
+  };
 
   return (
     <div className="p-5 border shadow-md rounded-md bg-white">
@@ -31,13 +39,21 @@ const FormSection = ({ selectedTemplate }: PROPS) => {
           <div className="my-5 flex gap-2 flex-col">
             <label className="font-bold">{item.label}</label>
             {item.field == "input" ? (
-              <Input />
+              <Input
+                name={item.name}
+                required={item.required}
+                onChange={handleInputChange}
+              />
             ) : (
-              item.field == "textarea" && <Textarea />
+              item.field == "textarea" && (
+                <Textarea name={item.name} onChange={handleInputChange} />
+              )
             )}
           </div>
         ))}
-        <Button type="submit" className="py-6 w-full">Generate Content</Button>
+        <Button type="submit" className="py-6 w-full">
+          Generate Content
+        </Button>
       </form>
     </div>
   );
